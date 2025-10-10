8ba6824a5b5c91c17adb4bf576fba2fe051a8adb
ahhfzwl <ahhfzwl@qq.com>
2025-10-10 16:22:29 +0800

    Update upnp.sh

--- upnp.sh
+++ upnp.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-Public_addr="$1"
-Public_port="$2"
-IP4P="$3"
-Bind_port="$4"
-Protocol="$5"
-Private_addr="$6"
-Private_port="80"
 
-router_url="http://192.168.1.1:52869/upnp/control/WANIPConn1"
+# 参数与默认值
+Public_addr="${1:-192.168.1.100}"    # 默认公网地址（如果需要可改）
+Public_port="${2:-8080}"             # 默认外部端口
+IP4P="${3:-192.168.1.100}"           # 默认内网IP
+Bind_port="${4:-8080}"               # 默认绑定端口
+Protocol="${5:-TCP}"                 # 默认协议
+Private_addr="${6:-192.168.1.100}"   # 默认私网地址
+Private_port="${7:-80}"              # 默认私网端口
+router_url="${8:-http://192.168.1.1:52869/upnp/control/WANIPConn1}" # 默认路由器UPnP控制URL
 
+# 执行UPnP添加端口映射
 curl -s -X POST "$router_url" \
      -H "Content-Type: text/xml; charset=utf-8" \
      -H "SOAPAction: \"urn:schemas-upnp-org:service:WANIPConnection:1#AddPortMapping\"" \
@@ -18,7 +20,7 @@ curl -s -X POST "$router_url" \
     <u:AddPortMapping xmlns:u=\"urn:schemas-upnp-org:service:WANIPConnection:1\">
       <NewRemoteHost></NewRemoteHost>
       <NewExternalPort>${Bind_port}</NewExternalPort>
-      <NewProtocol>${Protocol^^}</NewProtocol>
+      <NewProtocol>$(echo "$Protocol" | tr 'a-z' 'A-Z')</NewProtocol>
       <NewInternalPort>${Private_port}</NewInternalPort>
       <NewInternalClient>${Private_addr}</NewInternalClient>
       <NewEnabled>1</NewEnabled>
