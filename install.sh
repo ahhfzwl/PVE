10efd2f1346ac0cc42c0f616d678fd32f1124e88
ahhfzwl <ahhfzwl@qq.com>
2025-10-25 15:44:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
-# bash <(curl -Ls https://raw.sock.cf/ahhfzwl/Shell/main/install.sh)
+set -e
+export DEBIAN_FRONTEND=noninteractive
 
-install_packages() {
-  local packages=("cron" "htop" "openssh-server" "nano" "wget" "curl" "screen" "iputils-ping" "net-tools" "dnsutils" "iproute2" "tcptraceroute" "bc" "socat" "systemctl")
-  local to_install=()
-  for pkg in "${packages[@]}"; do
-    if ! dpkg -l | grep -q "^ii  $pkg "; then
-      to_install+=("$pkg")
-    fi
-  done
+sh -c 'echo "
+deb http://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free-firmware non-free
+deb-src http://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free-firmware non-free
+deb http://mirrors.tuna.tsinghua.edu.cn/debian-security/ bookworm-security main contrib non-free-firmware non-free
+deb-src http://mirrors.tuna.tsinghua.edu.cn/debian-security/ bookworm-security main contrib non-free-firmware non-free
+deb http://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free-firmware non-free
+deb-src http://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free-firmware non-free
+" > /etc/apt/sources.list'
+rm -rf /etc/apt/sources.list.d
 
-  if [ "${#to_install[@]}" -eq 0 ]; then
-    echo "所有软件包均已安装。"
-  else
-    local package_list="${to_install[*]}"
-    echo "安装软件包: $package_list"
-    apt update && apt -y install $package_list
-  fi
-}
+apt-get update && \
+apt-get install -y ca-certificates cron nano wget iputils-ping net-tools iproute2 dnsutils socat openssh-server htop unzip locales tzdata
 
-install_packages
+echo "Asia/Shanghai" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+locale-gen zh_CN.UTF-8
+update-locale LANG=zh_CN.UTF-8
+export LANG=zh_CN.UTF-8
+
+echo 'root:1224' | chpasswd
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+service cron start
+service ssh start
+
+if ! grep -q '# AUTO_START_CRON_SSH' /root/.bashrc; then
+cat >> /root/.bashrc <<\EOF
+
+# AUTO_START_CRON_SSH
+if [ "$$" -eq 1 ]; then
+    service cron start
+    service ssh start
+    export LANG=zh_CN.UTF-8
+fi
+EOF
+fi
