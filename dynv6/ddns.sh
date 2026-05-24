e7dc1955f9ad2e32cb29567f11b9bea20f2250f1
zhzwl <ahhfzwl@qq.com>
2026-05-24 22:30:33 +0800

    Fix script to reference ddns.sh instead of dynv6.sh

--- dynv6/ddns.sh
+++ dynv6/ddns.sh
@@ -3,7 +3,7 @@ HOST=$1
 FILE=$HOME/.dynv6.addr6
 [ -e $FILE ] && OLD=`cat $FILE`
 if [ -z "$HOST" -o -z "$TOKEN" ]; then
-  echo "TOKEN=ddtVfZgfE19REdPwy2jThrfCm58URR ./dynv6.sh tunnels.dynv6.net"
+  echo "TOKEN=ddtVfZgfE19REdPwy2jThrfCm58URR ./ddns.sh tunnels.dynv6.net"
   exit 1
 fi
 NEW=$(ip -6 addr list scope global $device | grep -v " fd" | sed -n 's/.*inet6 \([0-9a-f:]\+\).*/\1/p' | head -n 1)
