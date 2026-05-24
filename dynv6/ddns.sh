f08d928e0579e3157a852ef031bd9a079035004d
zhzwl <ahhfzwl@qq.com>
2026-05-25 00:03:49 +0800

    Update echo command for TOKEN in ddns.sh

--- dynv6/ddns.sh
+++ dynv6/ddns.sh
@@ -3,11 +3,11 @@ HOST=$1
 FILE=$HOME/.dynv6.addr6
 [ -e $FILE ] && OLD=`cat $FILE`
 if [ -z "$HOST" -o -z "$TOKEN" ]; then
-  echo "TOKEN=ddtVfZgfE19REdPwy2jThrfCm58URR ./ddns.sh tunnels.dynv6.net"
+  echo "TOKEN=ddtVfZgfE19REdPwy2jThrfCm58URR ./ddns.sh xtunnel.dynv6.net"
   exit 1
 fi
 NEW=$(ip -6 addr list scope global $device | grep -v " fd" | sed -n 's/.*inet6 \([0-9a-f:]\+\).*/\1/p' | head -n 1)
 if [ -z "$NEW" ]; then echo "no IPv6 address found"; exit 1; fi
 if [ "$OLD" = "$NEW" ]; then echo "IPv6 address unchanged"; exit; fi
-curl -fsS "http://dynv6.com/api/update?hostname=$HOST&ipv6prefix=auto&token=$TOKEN"
+curl "http://dynv6.com/api/update?hostname=$HOST&ipv6prefix=auto&token=$TOKEN"
 echo $NEW > $FILE
