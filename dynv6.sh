f3a8ddf3f19e85afb7f2dd55d8e19b46ba1269d5
zhzwl <ahhfzwl@qq.com>
2026-05-24 22:08:54 +0800

    Create dynv6.sh

---
+++ dynv6.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+TOKEN=ddtVfZgfE19REdPwy2jThrfCm58URR ./dynv6.sh tunnels.dynv6.net
+HOST=$1
+FILE=$HOME/.dynv6.addr6
+[ -e $FILE ] && OLD=`cat $FILE`
+if [ -z "$HOST" -o -z "$TOKEN" ]; then
+  echo "Usage: token=<your-authentication-token> [netmask=64] $0 your-name.dynv6.net [device]"
+  exit 1
+fi
+NEW=$(ip -6 addr list scope global $device | grep -v " fd" | sed -n 's/.*inet6 \([0-9a-f:]\+\).*/\1/p' | head -n 1)
+if [ "$OLD" = "$NEW" ]; then
+  echo "IPv6 address unchanged"
+  exit
+fi
+curl "http://dynv6.com/api/update?hostname=$hostname&ipv6prefix=auto&token=$token"
+echo $current > $file
