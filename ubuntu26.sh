dc6724fe13b4eff8e6c7542002af1af881765c6b
zhzwl <ahhfzwl@qq.com>
2026-05-25 20:34:30 +0800

    Update ubuntu26.sh

--- ubuntu26.sh
+++ ubuntu26.sh
@@ -1,5 +1,34 @@
+#!/bin/bash -e
+
 apt update
 export DEBIAN_FRONTEND=noninteractive
-# systemctl出错原因 gpg
-apt install -y openssh-server cron curl sudo nano wget iputils-ping net-tools iproute2 dnsutils socat htop unzip locales tzdata
-echo 'root:1224' | chpasswd && sed -i 's/^#\?Port .*/Port 2095/g;s/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config && service ssh restart
+apt install -y curl sudo gpg cron nano wget iputils-ping net-tools iproute2 dnsutils socat openssh-server htop unzip locales tzdata 
+
+echo 'zh_CN.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen zh_CN.UTF-8
+echo 'LANG=zh_CN.UTF-8' > /etc/default/locale
+export LANG=zh_CN.UTF-8
+. /etc/default/locale
+
+ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+echo "Asia/Shanghai" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+echo 'root:1224' | chpasswd
+sed -i 's/^#\?Port .*/Port 22/g' /etc/ssh/sshd_config
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+rm -r /etc/ssh/sshd_config.d
+/etc/init.d/cron restart
+/etc/init.d/ssh restart
+
+if ! grep -q '# AUTO_START' ~/.bashrc; then
+cat >> ~/.bashrc <<'EOF'
+
+# AUTO_START
+if [ "$$" -eq 1 ]; then
+    /etc/init.d/cron restart
+    /etc/init.d/ssh restart
+    export LANG=zh_CN.UTF-8
+fi
+EOF
+fi
