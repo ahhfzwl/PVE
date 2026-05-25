ef8e9f96ce49a8403c062b186098b0bc62f978cb
zhzwl <ahhfzwl@qq.com>
2026-05-25 12:39:41 +0800

    Create ubuntu26.sh

---
+++ ubuntu26.sh
@@ -0,0 +1,3 @@
+apt update
+export DEBIAN_FRONTEND=noninteractive
+apt install -y curl sudo gpg cron nano wget iputils-ping net-tools iproute2 dnsutils socat openssh-server htop unzip locales tzdata
