936479ae95d5436ad14fac2a46bf3d225aba1d2d
zhzwl <ahhfzwl@qq.com>
2026-05-29 12:57:49 +0800

    Update debian13.sh

--- debian13.sh
+++ debian13.sh
@@ -5,15 +5,8 @@ apt update
 export DEBIAN_FRONTEND=noninteractive
 apt install -y cron openssh-server wget curl sudo gpg htop unzip locales tzdata nano iputils-ping net-tools iproute2 dnsutils socat
 
-echo 'zh_CN.UTF-8 UTF-8' >> /etc/locale.gen
-locale-gen zh_CN.UTF-8
-echo 'LANG=zh_CN.UTF-8' > /etc/default/locale
-export LANG=zh_CN.UTF-8
-. /etc/default/locale
-
-ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-echo "Asia/Shanghai" > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
+echo 'zh_CN.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen && echo 'LANG=zh_CN.UTF-8' > /etc/default/locale && export LANG=zh_CN.UTF-8
+ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
 
 echo 'root:1224' | chpasswd
 sed -i 's/^#\?Port .*/Port 22/g' /etc/ssh/sshd_config
