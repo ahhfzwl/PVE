4e9a51599f5b8bf6b088aab2cf9b834db872f7bb
zhzwl <ahhfzwl@qq.com>
2026-06-23 07:41:03 +0800

    Update install.sh

--- GoogleCloudServer/install.sh
+++ GoogleCloudServer/install.sh
@@ -2,5 +2,5 @@
 
 apt update
 apt install -y ipset vnstat
-CRON_JOB="* * * * * vnstat -i ens4 --alert 0 3 m tx 200 GiB || /sbin/poweroff"
+CRON_JOB="* * * * * /usr/bin/vnstat -i ens4 --alert 0 3 m tx 200 GiB || /usr/sbin/poweroff"
 ( crontab -l 2>/dev/null | grep -v "$CRON_JOB" ; echo "$CRON_JOB" ) | crontab -
