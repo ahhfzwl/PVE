2d89f8639e8f7ddeb047369509150b579907cedc
ahhfzwl <54196143+ahhfzwl@users.noreply.github.com>
2026-07-07 11:27:58 +0800

    Replace ipset with iptables-persistent in install script

--- GoogleCloudServer/install.sh
+++ GoogleCloudServer/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt update
-apt install -y ipset vnstat
+apt install -y iptables-persistent vnstat
 CRON_JOB="* * * * * vnstat -i ens4 --alert 0 3 m tx 200 GiB || /sbin/poweroff"
 ( crontab -l 2>/dev/null | grep -v "$CRON_JOB" ; echo "$CRON_JOB" ) | crontab -
